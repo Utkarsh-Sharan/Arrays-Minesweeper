61602376ec0610f9b7b7b660b525d68e1f65b234
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-09-16 11:11:17 +0530

    connected board service with service locator

--- Array-Minesweeper/header/Gameplay/Board/BoardService.h
+++ Array-Minesweeper/header/Gameplay/Board/BoardService.h
@@ -4,9 +4,24 @@ namespace Gameplay
 {
 	namespace Board
 	{
+		class BoardController;
+
 		class BoardService
 		{
+		private:
+			BoardController* board_controller;
 
+			void destroy();
+
+		public:
+			BoardService();
+			~BoardService();
+
+			void initialize();
+			void update();
+			void render();
+
+			void resetBoard();
 		};
 	}
 }
