a44ea70794cb65d92f135eff6b681a053f3fd193
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-09-17 10:25:28 +0530

    cell model setup

--- Array-Minesweeper/source/Gameplay/Cell/CellModel.cpp
+++ Array-Minesweeper/source/Gameplay/Cell/CellModel.cpp
@@ -4,6 +4,52 @@ namespace Gameplay
 {
 	namespace Cell
 	{
+		CellModel::CellModel()
+		{
 
+		}
+
+		CellModel::~CellModel() = default;
+
+		void CellModel::initialize()
+		{
+
+		}
+
+		void CellModel::update()
+		{
+
+		}
+
+		void CellModel::render()
+		{
+
+		}
+
+		CellState CellModel::getCellState()
+		{
+			return cell_state;
+		}
+
+		void CellModel::setCellState(CellState state)
+		{
+			cell_state = state;
+		}
+
+		CellValue CellModel::getCellValue()
+		{
+			return cell_value;
+		}
+
+		void CellModel::setCellValue(CellValue value)
+		{
+			cell_value = value;
+		}
+
+		void CellModel::reset()
+		{
+			cell_value = CellValue::EMPTY;
+			cell_state = CellState::HIDDEN;
+		}
 	}
 }
