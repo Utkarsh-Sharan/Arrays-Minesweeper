cda910f51f576119321992a7cd68797a6d8ca861
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-09-18 12:27:43 +0530

    added new font for gameplay ui

--- Array-Minesweeper/source/UI/Gameplay/GameplayUIController.cpp
+++ Array-Minesweeper/source/UI/Gameplay/GameplayUIController.cpp
@@ -49,12 +49,12 @@ namespace UI
 
 		void GameplayUIController::initializeMineText()
 		{
-			mine_text->initialize("000", sf::Vector2f(mine_text_left_offset, mine_text_top_offset), FontType::ROBOTO, font_size, text_color);
+			mine_text->initialize("000", sf::Vector2f(mine_text_left_offset, mine_text_top_offset), FontType::DS_DIGIB, font_size, text_color);
 		}
 
 		void GameplayUIController::initializeTimeText()
 		{
-			time_text->initialize("000", sf::Vector2f(time_text_left_offset, time_text_top_offset), FontType::ROBOTO, font_size, text_color);
+			time_text->initialize("000", sf::Vector2f(time_text_left_offset, time_text_top_offset), FontType::DS_DIGIB, font_size, text_color);
 		}
 
 		void GameplayUIController::initializeButton()
