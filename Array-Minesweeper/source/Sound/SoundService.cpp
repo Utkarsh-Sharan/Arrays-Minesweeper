ffe0378bc7e43595f5cf2fc51d381dfb27f6629b
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-09-19 11:08:07 +0530

    modified sound service

--- Array-Minesweeper/source/Sound/SoundService.cpp
+++ Array-Minesweeper/source/Sound/SoundService.cpp
@@ -21,6 +21,9 @@ namespace Sound
 	{
 		if (!buffer_button_click.loadFromFile(Config::button_click_sound_path))
 			printf("Error loading background music file");
+
+		if (!buffer_explosion.loadFromFile(Config::explosion_sound_path))
+			printf("Error loading background music file");
 	}
 
 	void SoundService::playSound(SoundType soundType)
@@ -30,6 +33,11 @@ namespace Sound
 		case SoundType::BUTTON_CLICK:
 			sound_effect.setBuffer(buffer_button_click);
 			break;
+
+		case SoundType::EXPLOSION:
+			sound_effect.setBuffer(buffer_explosion);
+			break;
+
 		default:
 			printf("Invalid sound type");
 			return;
